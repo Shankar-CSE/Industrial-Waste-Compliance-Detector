dc06bc01a20760b0af15b931e9e441a67cf6328d
Shankar-CSE <shankar.l5252@gmail.com>
2025-10-12 16:57:56 +0530

    Remove REM comments from run.bat to enable dataset preparation and feature selection scripts

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 
-@REM python preprocessing/datasetmaker.py
-@REM python preprocessing/selecting_features.py
+python preprocessing/datasetmaker.py
+python preprocessing/selecting_features.py
 python preprocessing/preprocessing.py
 python Training_model/train.py
 python app.py
